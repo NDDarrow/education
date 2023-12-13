2c0dbba1a3217344ad820184bbb288bcfa75c35f
LEE-JI-HO <wlgh0818@naver.com>
2023-12-13 18:12:57 +0900

    0612

--- 과제/static/js/board.js
+++ 과제/static/js/board.js
@@ -69,7 +69,6 @@ let player_list = new Array();// 게임 참가자
 //         ));
 //     }
 // };
-    //console.log(zone)
 //구역객채들을 zone클래스 div에 적용하기
 function zone_draw(){
     $.each( zone, function( idx , obj){
@@ -80,7 +79,9 @@ function zone_draw(){
         }else{
             $(".zone").eq(idx).children(".zone_name").text(obj.name)
             $(".zone").eq(idx).children(".zone_color").css("background-color",obj.color);
+            //각 구역의 번호 부여
         }
+        $(".zone").eq(idx).attr("data-num" , obj.num)
     });
 };
 
@@ -88,8 +89,10 @@ function game_init(){
     var pc = Number( $("#player_number").val() );
 
     $("#game_state").html("<h3>게임현황</h3>");
+
+    var pcolor=["#ff0000","#00ff00","#FFFF00","#0000FF","#00BBFF"]
     for(var i = 1; i <= pc; i++){
-        player_list.push( new player( i , "#ff0000"));
+        player_list.push( new player( i , pcolor[i-1]));
         $("#game_state").append(
             `<div class='ps'>
                 <b class='pnum'>${i}</b>
@@ -101,34 +104,158 @@ function game_init(){
             </div>`
         );
     }
+    $("input[type=color]").on("change", change_pcl );
     $("#game_state").show();
     $("#set_player").hide();
+
+
+    //게임 플레이어수에 맞는 말 만들기
+    //svg - scalable vector graphics (수학공식을 통해 이미지를 저장, 표시)
+    //<i class="fa-regular fa-user" style="color: #ff0000;"></i> 레드
+    for(var i  = 0; i < player_list.length; i++){
+        var gamer = player_list[i];
+        var zone_location = find_location( gamer.location)
+        var tag=
+            `<div class='meeple m${gamer.num}' data-pn='${gamer.num}'
+            style='color:${gamer.color};'>
+            <i class="fa-solid fa-user"></i>
+            </div>`;
+        $(".zone").eq(zone_location).append(tag);
+        
+        overlap(zone_location)
+        
+    }
+    // var idx = find_location( 0 );
+    // console.log(idx)
+    
+    create_dice();
 };
 
-function change_pcl(){
+function create_dice(){ //화면에 주사위 나타내기
+    var dice = `
+    <div id='dice_wrap'>
+        <div class='dice'>
+            <div class='diceImg'>
+                <img id='dice1' src='./static/images/dice1.png'>
+            </div>
+            <div class='diceImg'>
+                <img id='dice2' src='./static/images/dice4.png'>
+            </div>
+        </div>
+        <div class='dicebt'>
+            <button onclick='rolling(this)'>굴리기</button>
+        </div>
+    </div>
+    `;
+    $(".center").append(dice);
+}
+
+function overlap(location){ //한자리에 말이 중복되면 겹침을 방지하기 위한 함수
+    var len = $(".zone").eq(location).children(".meeple").length;
+    if( len >= 2){
+        var left= 50, top=50;
+        for(var i = 0; i <= len; i++){
+            $(".zone").eq(location).children(".meeple").eq(i).css("left",(left+i*5)+"%");
+            $(".zone").eq(location).children(".meeple").eq(i).css("top",(top+i*5)+"%");
+        }
+    }
+};
+
+    
+
+function find_location( n ){ //플레이어 말이 표시될 위치 또는 이동할 위치 찾기
+    var index = 0;
+    $(".zone").each( function( idx, item){
+        var num = Number($(item).data("num")); // zone클래스 태그의 data-num값
+        if(num == n){
+            index = idx;
+            return; //return은 해당함수를 종료시키기도한다.
+            //each안에 만든 익명함수만 종료시키기 때문에 find_location 함수를 통해 return하는게 불가능하다.
+            //그래서 값을 index변수에 저장하여 index변수를 반환 시켜준 것이다.
+        }
+    });
+    return index; // data-num 과 n의 값이 일치하는 태그의 위치넘기기
+}
+
+function change_pcl(){ //플레이어가 자신의 말 색상을 변경할 경우 실행
+    var new_color = $(this).val();
+    var num = Number($(this).attr("id").substring(3)); // attr은 태그의속성 - id .class.name 등등
+    var gamer = player_list[num-1];;//색상 변경한 플레이어 객체
+
+    gamer.color = new_color;
+    //자신의 말 찾기 num변수에는 자신의 번호가 저장되있다.
+    for( var i = 0; i < $(".meeple").length; i++){
+        if( $(".meeple").eq(i).data("pn") == num){
+            $(".meeple").eq(i).css("color",new_color)
+            break; //플레이어말 찾아서 색변경시 반복문 종료
+        }
+    }
+    
 
 }
 
 
 
 $(function(){
-    //zone_create();
-    zone_draw();
+    //현재상황 - json파일용은 어제 다루었던 zone배열안의 내용과 같다.
+    //getJSON으로 json파일을 읽어와서 zone배열에 저장
+    // zone배열의 내용은 어제와 동일
+    // zone_draw() 함수는 zone 배열의 값을 가지고와서 화면에 표시한다.
+    // 하지만 zone_draw() 함수가 정상적으로 작동하지 않고있음
+    // getJSON의 function(data){}안으로 넣어주면 작동됨 
+    // 비동기문제가 발생하기 때문
+    // 비동기처리 코드가 실행이 완료되면 그 다음 실행 될 수 있게 하는방법
+    // async, await
+    // async는 비동기 함수 앞에 붙여준다
+    // await는 비동기 처리 앞에 붙여준다.
+    // async function a(){ await.$getJSON();}
 
-    $.getJSON("./json/city.json", function(data){
-        zone = data
-        zone_draw(zone)
+    // zone_create();
+    $.getJSON("./data/city.json", function(data){
+        zone = data;
+        zone_draw()
+        
     });
 
+    // zone_draw();
+    
     $("#enroll").on("click", game_init );
     $("#player_number").on("change", function(){
         $(this).next().text( $(this).val() + "명")
     });
     $("#player_number + label").text(2+"명");
 
-    $("input[type=color]").on("change", change_pcl );
 });
+// 16 -복지기금 , 24-공항 , 28-기금납부 , 8-무인도, 0-출발지
+function func_link(){
+    zone[0].func = welfare;
+    zone[8].func = airport;
+    zone[16].func = fundpayment;
+    zone[23].func = island;
+    zone[31].func = complete;
+}
 
+function welfare(gamer){ //복지기금 수령
+    alert(`복지기금 ${fund}만원을 받았습니다.`)
+    gamer.money += fund;
+    fund = 0;
+    $("#pm"+gamer.num).text( gamer.money+"만원");
+}
+function airport(gamer){ // 원하는 위치로 이동
+
+}
+function fundpayment(){ //복지기금 납부
+    alert("복지기금으로 20만원을 납부했습니다.")
+    gamer.money -= 20;
+    fund += 20;
+    $("#pm"+gamer.num).text( gamer.money+"만원");
+}
+function island(){ //3턴동안 못움직임
+
+}
+function complete(){ //출발지에 도착하거나 통과하면 20만원 보너스
+
+}
 
 
 // 과제 - 각 구역의 객체를 json 으로 작성해 오세요.
